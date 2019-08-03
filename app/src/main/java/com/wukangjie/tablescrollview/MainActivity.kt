b6d6b0b8bc76362d508842aa3c9f94eddeefa75f
wkj093 <wkj093@qimingpian.com>
2019-08-03 10:52:23 +0800

    基础框架上传

---
+++ app/src/main/java/com/wukangjie/tablescrollview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wukangjie.tablescrollview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
